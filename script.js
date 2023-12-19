8819db7898488a1fc92935c76c6fdb119f33df8c
realsulty <Sultanobm1@gmail.com>
2023-12-19 12:20:52 +0000

    fixed api

--- script.js
+++ script.js
@@ -93,7 +93,7 @@
 
 function fetchUser() {
  showSpinner();
- fetch('https://randomuser.me/api1')
+ fetch('https://randomuser.me/api')
  .then((res) => {
   if (!res.ok) {
 throw new Error('Request Failed');
